0e968ecc1a5726fc14f2a589513a6687150ff9dd
AYUSH KUMAR SINGH <ayushkumarsingh0708@gmail.com>
2024-05-02 23:38:43 +0530

    added plane and color fixes

--- CG_PROJECT/CG_PROJECT.cpp
+++ CG_PROJECT/CG_PROJECT.cpp
@@ -4,10 +4,10 @@
 #define GL_SILENCE_DEPRECATION
 #include<iostream>
 using namespace std;
-float tmp[] = { 0,0,1 },tmp2[] = { 0,1,0 },bridgeY = 0.0,shipX=-50.0,shipY=-50.0,bridgeWidth = 300.0,trainX=0.0,
-bridgeColor[] = { 0.4, 0.2, 0.05 }, frontColor[] = { 0, 0.5, 0.2 },supportColor[] = {0.5, 0.5, 1 },
+float tmp[] = { 0.2,0.2,0.2 },tmp2[] = { 0,1,0 },bridgeY = 0.0,shipX=-50.0,shipY=-50.0,bridgeWidth = 300.0,trainX=0.0,
+bridgeColor[] = { 0.3, 0.2, 0.05 }, frontColor[] = { 0.7, 0.5, 0.2 }, supportColor[] = { 0.5, 0.5, 1 }, sideColor[] = {0.5,0.3,.2},
 greyColor[] = { 0.4,0.4,0.4 }, redColor[] = {1,0,0},greenColor[]={0,1,0},
-blackColor[]={0,0,0}, c1 = 0.0,c2=0.0,c3=0.0,c4=0;
+blackColor[]={0,0,0}, c1 = 0.0,c2=0.0,c3=0.0,c4=0,planeX=0,planeY=0;
 bool isShipGone = false,isBridgeUp=false;
 int sides = 360,pos1=90,pos2=45,pos3=270;
 const float radius = 600.0f;
@@ -18,7 +18,7 @@ void myInit(void) {
 	glClearColor(0,0.0,0.2, 0.0);
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
-	glOrtho(0, 1500, 000, 1000,1500,-2000);//viewport
+	glOrtho(0, 1500,00,1000,1500,-2000);//viewport
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
 }
@@ -63,8 +63,8 @@ void getTop(int w, int x1, int y1, int x2, int y2,float color[]) {
 	glEnd();
 
 }
-void getSide(int h, int x1, int y1, int x2, int y2) {
-	glColor3f(0.5, 0.5, 0.2);
+void getSide(int h, int x1, int y1, int x2, int y2,float color[]) {
+	glColor3f(color[0], color[1], color[2]);
 	glBegin(GL_QUADS);
 	glVertex2f(x1, y1);
 	glVertex2f(x2, y2);
@@ -73,12 +73,12 @@ void getSide(int h, int x1, int y1, int x2, int y2) {
 	glEnd();
 
 }
-void drawBase(int h, int w, int x1, int y1, int x2, int y2) {
+void drawBase(int h, int w, int x1, int y1, int x2, int y2,float frontCol[], float sideCol[],float topCol[]) {
 	glBegin(GL_QUADS);
 	glColor3f(0, 0.5, 0.2);
-	getTop(w, x1, y1 + h, x2, y2 + h,bridgeColor);
-	getSide(h, x1, y1, x2, y2);
-	drawFront(h, w, x1, y1, frontColor);
+	getTop(w, x1, y1 + h, x2, y2 + h,topCol);
+	getSide(h, x1, y1, x2, y2,sideCol);
+	drawFront(h, w, x1, y1, frontCol);
 	glEnd();
 }
 void drawTriangles(int x1, int y1, int x2, int y2, int x3, int y3, float curentColor[3]) {
@@ -99,11 +99,11 @@ void drawSupportBeams(int x1, int y1, int x2, int y2, int x3, int y3, int x4, in
 	glVertex2f(x4, y4);
 	glEnd();
 }
-void drawVertLog(int h, int a1, int b1, int w, float color[3]) {
-	getSide(h, a1 + w - 3, b1 + 3, a1 + w, b1);
-	drawFront(h, w, a1 - 3, b1 + 3, tmp2);
-	getSide(h, a1 - 3, b1 + 3, a1, b1);
-	drawFront(h, w, a1, b1, color);
+void drawVertLog(int h, int a1, int b1, int w, float side[],float front[],float front2[]) {
+	getSide(h, a1 + w - 3, b1 + 3, a1 + w, b1,side);
+	drawFront(h, w, a1 - 3, b1 + 3, front2);
+	getSide(h, a1 - 3, b1 + 3, a1, b1,side);
+	drawFront(h, w, a1, b1, front);
 }
 void ocean() {
 	glBegin(GL_QUADS);
@@ -117,13 +117,14 @@ void ocean() {
 }
 void railway() {
 	getTop(650, 0, 250, -50, 280, bridgeColor);
-	getTop(50, 600, 250, 550, 280, tmp2);
+	float col[] = { .4,0.4,0.4 }, col2[] = { 0.25,0.25,0.25 }, col3[] = { 0.6,0.6,0.6 };
+	getTop(50, 600, 250, 550, 280, col);
 	float front[] = { 0.56, 0.39, 0.05 };
-	drawFront(60, 650, 0, 190, front);
+	drawFront(60, 650, 0, 190, col2);
 	getTop(1000, 850, 250, 800, 280, bridgeColor);
-	getTop(50, 850, 250, 800, 280, tmp2);
-	getSide(60, 850, 190, 800, 220);
-	drawFront(60, 1000, 850, 190, front);
+	getTop(50, 850, 250, 800, 280, col);
+	getSide(60, 850, 190, 800, 220,col3);
+	drawFront(60, 1000, 850, 190, col2);
 
 }
 void railTrack() {
@@ -171,58 +172,57 @@ void railTrack() {
 	glEnd();
 }
 void boat(int s, int x, int y) {
-	frontColor[0] = 0;frontColor[1] = 0.5;frontColor[2] = 0.2;
-	drawBase(25 * s, 55 * s, 180 * s + x, 200 * s + y, 130 * s + x, 250 * s + y);	//bottom-base
-	drawBase(18 * s, 50 * s, 180 * s + x, 228 * s + y, 136 * s + x, 272 * s + y);//second-base
-	drawBase(14 * s, 45 * s, 180 * s + x, 248 * s + y, 142 * s + x, 286 * s + y);//top-base
+	float col1[] = {0.7,0.7,0.7}, col2[] = { 0.647f, 0.165f, 0.165f }, col3[] = {0.4,0.4,0.4};
+	drawBase(25 * s, 55 * s, 180 * s + x, 200 * s + y, 130 * s + x, 250 * s + y,col1,col3,col2);	//bottom-base
+	drawBase(18 * s, 50 * s, 180 * s + x, 228 * s + y, 136 * s + x, 272 * s + y,col1,col3,col2);//second-base
+	drawBase(14 * s, 45 * s, 180 * s + x, 248 * s + y, 142 * s + x, 286 * s + y,col1,col3,col2);//top-base
 	//top-base-cuboid
-	drawFront(70 * s, 20 * s, 162 * s + x, 290 * s + y, frontColor);
-	getSide(70 * s, 190 * s + x, 280 * s + y, 182 * s + x, 290 * s + y);
-	getSide(70 * s, 170 * s + x, 280 * s + y, 162 * s + x, 290 * s + y);
-	frontColor[0] = 2;
-	drawFront(70 * s, 20 * s, 170 * s + x, 280 * s + y, frontColor);
-	frontColor[2] = 0.6;
+	drawFront(70 * s, 20 * s, 162 * s + x, 290 * s + y,col3);
+	col3[2] = 0.6;
+	getSide(70 * s, 190 * s + x, 280 * s + y, 182 * s + x, 290 * s + y,col3);
+	getSide(70 * s, 170 * s + x, 280 * s + y, 162 * s + x, 290 * s + y,col3);
+	drawFront(70 * s, 20 * s, 170 * s + x, 280 * s + y, col1);
 	//boat-front-design
-	drawTriangles(235 * s + x, 200 * s + y, 235 * s + x, 225 * s + y, 265 * s + x, 218 * s + y, frontColor);
+	drawTriangles(235 * s + x, 200 * s + y, 235 * s + x, 225 * s + y, 265 * s + x, 218 * s + y, col1);
 	float tmpColor[] = { 0,0,0 };
 	drawTriangles(180 * s + x, 200 * s + y, 235 * s + x, 200 * s + y, 265 * s + x, 218 * s + y, tmpColor);
-	frontColor[2] = 0;frontColor[1] = 0.5;frontColor[0] = 1;
-	drawTriangles(180 * s + x, 200 * s + y, 180 * s + x, 225 * s + y, 265 * s + x, 218 * s + y, frontColor);
+	drawTriangles(180 * s + x, 200 * s + y, 180 * s + x, 225 * s + y, 265 * s + x, 218 * s + y, col3);
 }
 void drawRightBackBridge() {
 	tmp2[1] = 0.2;
 	int a1 = 850,b1 = 252;
 	drawSupportBeams(920, 280, 970, 280, 808, 525, 808, 545, supportColor);//side-support
 	a1 = 810;b1 = 276;
-	drawVertLog(250, a1, b1, 15, tmp);//lower-big-log
-	drawVertLog(40, 812, 540,15,tmp);//upper-smaller-log
+	drawVertLog(250, a1, b1, 15,sideColor,bridgeColor,tmp);//lower-big-log
+	drawVertLog(40, 812, 540,15, sideColor, bridgeColor, tmp);//upper-smaller-log
 	
 }
 void drawRightFrontBridge() {
 	tmp2[1] = 0.2;
 	int a1 = 850,b1 = 252;
 	drawSupportBeams(1000, 250, 1055, 250, 848, 490, 848, 520, supportColor);//side-support
-	drawVertLog(250, a1, b1, 15, tmp);//lower-big-log
-	drawVertLog(40, 850, 510, 15, tmp);//upper-smaller-log
-	drawBase(20, 20, 855, 485, 785, 542);//log-joining-both-ver-log
+	drawVertLog(250, a1, b1, 15, sideColor, bridgeColor, tmp);//lower-big-log
+	drawVertLog(40, 850, 510, 15, sideColor, bridgeColor, tmp);//upper-smaller-log
+	drawBase(20, 20, 855, 485, 785, 542, sideColor,frontColor,tmp);//log-joining-both-ver-log
 }
 void drawLeftBackBridge() {
 	int a1 = 595,b1 = 276;
-	drawVertLog(250, a1, b1, 15, tmp);//lower-big-log
+	drawVertLog(250, a1, b1, 15, sideColor, bridgeColor, tmp);//lower-big-log
 	drawSupportBeams(420, 280, 475, 280, 592, 525, 592, 545, supportColor);//side-support
-	drawVertLog(40, 595, 540, 15, tmp);;//upper-smaller-log
+	drawVertLog(40, 595, 540, 15, sideColor, bridgeColor, tmp);;//upper-smaller-log
 }
 void drawLeftFrontBridge() {
-	tmp2[1] = 0.2;
+	float col[] = { .4,0.4,0.4 }, col2[] = { 0.25,0.25,0.25 }, col3[] = { 0.6,0.6,0.6 };
 	int a1 = 633, b1 = 252;
-	drawVertLog(40, 633, 510, 15, tmp);//upper-smaller-log
+	drawVertLog(40, 633, 510, 15, sideColor, bridgeColor, tmp);//upper-smaller-log
 	a1 = 633; b1 = 252;
-	drawVertLog(250, a1, b1, 15, tmp);//lower-big-log
-	drawBase(20, 20, 635, 485, 570, 545);//log-joining-both-ver-log
+	drawVertLog(250, a1, b1, 15, sideColor, bridgeColor, tmp);//lower-big-log
+	drawBase(20, 20, 635, 485, 570, 545,  sideColor,frontColor , tmp);//log-joining-both-ver-log
 	drawSupportBeams(470, 250, 525, 250, 631, 490, 631, 510, supportColor);//side-support
 }
 void movingBackBridge() {
-	drawBase(5, 202, 645, 250,605, 275);
+	float col[] = { .4,0.4,0.4 }, col2[] = { 0.25,0.25,0.25 }, col3[] = { 0.6,0.6,0.6 };
+	drawBase(5, 202, 645, 250,605, 275, col2, col3, col);
 	glBegin(GL_LINES);
 	for (int i = 0; i < 4; i++) {
 		glColor3f(0, 0, 0);
@@ -236,43 +236,43 @@ void movingBackBridge() {
 	}
 	float fColor[] = { 0.3, 0.2, 0.1 };
 	
-	
-	drawVertLog(150, 788, 275, 20, fColor);
-	drawBase(15, 160, 628, 410, 625, 413);
-	drawVertLog(150, 608, 275, 20, fColor);
+	float  frontTmp[] = { 0.25, 0.0, 0.0 } ,sideTmp[] = { 0.5, 0.0, 0.0 };
+	drawVertLog(150, 788, 275, 20, sideTmp, frontTmp, bridgeColor);
+	drawBase(15, 160, 628, 410, 625, 413, col3, sideTmp, bridgeColor);
+	drawVertLog(150, 608, 275, 20, sideTmp, frontTmp, bridgeColor);
 	glEnd();
 	
 
 }
 void movingFrontBridge() {
-	float fColor[] = { 0.3, 0.2, 0.1 };
-	drawVertLog(150, 828, 255, 20, fColor);
-	drawBase(15, 160, 668, 390, 665, 393);
-	drawVertLog(150, 648, 255, 20, fColor);
+	float  frontTmp[] = { 0.25, 0.0, 0.0 }, col3[] = { 0.6,0.6,0.6 }, sideTmp[] = { 0.5, 0.0, 0.0 };
+	drawVertLog(150, 828, 255, 20, sideTmp, frontTmp, bridgeColor);
+	drawBase(15, 160, 668, 390, 665, 393,col3, sideTmp, bridgeColor);
+	drawVertLog(150, 648, 255, 20, sideTmp, frontTmp,bridgeColor);
 }
 void drawTrain(int x) {
 	glPushMatrix();
-	drawBase(70, 100, 1950 + x, 255, 1920 + x, 275);//sixth-box
+	drawBase(70, 100, 1950 + x, 255, 1920 + x, 275, frontColor, sideColor, tmp);//sixth-box
 	drawFront(35, 40, 1960 + x, 280, blackColor);
 	drawFront(35, 40, 2005 + x, 280, blackColor);
-	drawBase(70, 100, 1840 + x, 255, 1810 + x, 275);//fifth-box
+	drawBase(70, 100, 1840 + x, 255, 1810 + x, 275, frontColor, sideColor, tmp);//fifth-box
 	drawFront(35, 40, 1850 + x, 280, blackColor);
 	drawFront(35, 40, 1895 + x, 280, blackColor);
-	drawBase(70, 100, 1730 + x, 255, 1700 + x, 275);//fourth-box
+	drawBase(70, 100, 1730 + x, 255, 1700 + x, 275, frontColor, sideColor, tmp);//fourth-box
 	drawFront(35, 40, 1740 + x, 280, blackColor);
 	drawFront(35, 40, 1785 + x, 280, blackColor);
-	drawBase(70, 100, 1620 + x, 255, 1590 + x, 275);//third-box
+	drawBase(70, 100, 1620 + x, 255, 1590 + x, 275, frontColor, sideColor, tmp);//third-box
 	drawFront(35, 40, 1630 + x, 280, blackColor);
 	drawFront(35, 40, 1675 + x, 280, blackColor);
-	drawBase(70, 100, 1510 + x, 255, 1480 + x, 275);//second-box
+	drawBase(70, 100, 1510 + x, 255, 1480 + x, 275, frontColor, sideColor, tmp);//second-box
 	drawFront(35, 40, 1520 + x, 280, blackColor);
 	drawFront(35, 40, 1565 + x, 280, blackColor);
-	drawBase(70, 100, 1400 + x, 255, 1370 + x, 275);//first-box
+	drawBase(70, 100, 1400 + x, 255, 1370 + x, 275, frontColor, sideColor, tmp);//first-box
 	drawFront(35, 40, 1410 + x, 280, blackColor);
 	drawFront(35, 40, 1455 + x, 280, blackColor);
 	//train-engine
 	drawFront(35, 60, 1340 + x, 255, redColor);
-	getSide(35, 1340 + x, 255, 1305 + x, 275);
+	getSide(35, 1340 + x, 255, 1305 + x, 275,sideColor);
 	glColor3f(1, 1, 1);
 	glBegin(GL_QUADS);
 	glVertex2f(1340 + x, 290);
@@ -504,13 +504,13 @@ void drawCity() {
 	glPopMatrix();
 	glPopMatrix();
 	//buildings
-	drawBase(400, 60, 1400, 560, 1380, 610);
-	drawBase(200, 50, 1340, 500, 1320, 550);
-	drawBase(250, 70, 1430, 430, 1410, 480);
+	drawBase(400, 60, 1400, 560, 1380, 610, frontColor, sideColor, tmp);
+	drawBase(200, 50, 1340, 500, 1320, 550, frontColor, sideColor, tmp);
+	drawBase(250, 70, 1430, 430, 1410, 480, frontColor, sideColor, tmp);
 	
 }
 void drawFerrisWheel() {
-	drawBase(20, 170, 1030, 420, 980, 490);
+	drawBase(20, 170, 1030, 420, 980, 490, frontColor, sideColor, tmp);
 	glPushMatrix();
 	glRotatef(70.0f, 0.0f, 1.0f, 0.0f);
 	glPushMatrix();
@@ -534,10 +534,9 @@ void drawFerrisWheel() {
 	glPopMatrix();
 	glPopMatrix();
 }
-
 void lightPole() {
-	drawBase(150, 15, 1050, 275, 1045, 280);
-	drawBase(80, 35, 1040, 425, 1035, 430);
+	drawBase(150, 15, 1050, 275, 1045, 280, frontColor, sideColor, tmp);
+	drawBase(80, 35, 1040, 425, 1035, 430, frontColor, sideColor, tmp);
 	glPushMatrix();
 	glRotatef(30, 0, 1, 0);
 	//red-light
@@ -546,12 +545,222 @@ void lightPole() {
 	circle(15, 1223, 445,bridgeY!=0? greyColor : greenColor);
 	glPopMatrix();
 }
+void drawA(int x, int y, int height) {
+	glBegin(GL_LINE_STRIP);
+	glVertex2f(x, y);
+	glVertex2f(x, y + height);
+	glVertex2f(x + height / 2, y + height);
+	glVertex2f(x + height / 2, y + height / 2);
+	glVertex2f(x, y + height / 2);
+	glVertex2f(x + height / 2, y + height / 2);
+	glVertex2f(x + height / 2, y);
+	glEnd();
+}
+void drawD(int x, int y, int height) {
+	glBegin(GL_LINE_STRIP);
+	glVertex2f(x, y);
+	glVertex2f(x + height / 2, y+height/10);
+	glVertex2f(x + height / 2, y+9*height / 10);
+	glVertex2f(x, y + height);
+	glVertex2f(x, y );
+	glEnd();
+}
+void drawH(int x, int y, int height) {
+	glBegin(GL_LINE_STRIP);
+	glVertex2f(x, y);
+	glVertex2f(x, y + height);
+	glVertex2f(x, y + height / 2);
+	glVertex2f(x + height / 2, y + height / 2);
+	glVertex2f(x + height / 2, y + height);
+	glVertex2f(x + height / 2, y);
+	glEnd();
+}
+void drawI(int x, int y, int height) {
+	glBegin(GL_LINE_STRIP);
+	glVertex2f(x + height / 4, y);
+	glVertex2f(x + 3 * height / 4, y);
+	glVertex2f(x + height / 2, y);
+	glVertex2f(x + height / 2, y + height);
+	glVertex2f(x + height / 4, y + height);
+	glVertex2f(x + 3 * height / 4, y + height);
+	glEnd();
+}
+void drawM(int x, int y, int height) {
+	glBegin(GL_LINE_STRIP);
+	glVertex2f(x, y);
+	glVertex2f(x, y + height);
+	glVertex2f(x + height / 4, y + 5 * height / 8);
+	glVertex2f(x + 2 * height / 4, y + height);
+	glVertex2f(x + 2 * height / 4, y);
+	glEnd();
+
+}
+void drawP(int x, int y, int height) {
+	glBegin(GL_LINE_STRIP);
+	glVertex2f(x, y);
+	glVertex2f(x, y + height);
+	glVertex2f(x + height / 2, y + height);
+	glVertex2f(x + height / 2, y + height / 2);
+	glVertex2f(x, y + height / 2);
+	glEnd();
+
+}
+void drawR(int x, int y, int height) {
+	glBegin(GL_LINE_STRIP);
+	glVertex2f(x, y);
+	glVertex2f(x, y + height);
+	glVertex2f(x + height / 2, y+height);
+	glVertex2f(x + height / 2, y + height / 2);
+	glVertex2f(x, y + height/2);
+	glVertex2f(x + height / 2, y );
+	glEnd();
+	
+}
+void drawS(int x, int y, int height) {
+	glBegin(GL_LINE_STRIP);
+	glVertex2f(x + height / 2, y + height);
+	glVertex2f(x, y + height);
+	glVertex2f(x, y + height / 2);
+	glVertex2f(x + height / 2, y + height / 2);
+	glVertex2f(x + height / 2, y);
+	glVertex2f(x, y);
+	glEnd();
+}
+void drawT(int x, int y, int height) {
+	glBegin(GL_LINE_STRIP);
+	glVertex2f(x, y + height);
+	glVertex2f(x + height, y + height);
+	glEnd();
+	glBegin(GL_LINE_STRIP);
+	glVertex2f(x + height / 2, y);
+	glVertex2f(x + height / 2, y + height);
+	glEnd();
+}
+void drawU(int x, int y, int height) {
+	glBegin(GL_LINE_STRIP);
+	glVertex2f(x, y + height);
+	glVertex2f(x, y);
+	glVertex2f(x + height / 2, y);
+	glVertex2f(x + height / 2, y + height);
+	glEnd();
+}
+void drawY(int x, int y, int height) {
+	glBegin(GL_LINE_STRIP);
+	glVertex2f(x + height / 2, y);
+	glVertex2f(x + height / 2, y + height / 2);
+	glVertex2f(x + height / 4, y + height);
+	glVertex2f(x + height / 2, y + height / 2);
+	glVertex2f(x + 3 * height / 4, y + height);
+	glEnd();
+}
+void writeSiddharth(int startX, int startY, int height) {
+	drawS(startX, startY, height);
+	drawI(startX + 0.5 * height, startY, height);
+	drawD(startX + 1.5 * (height), startY, height);
+	drawD(startX + 2.5 * (height), startY, height);
+	drawH(startX + 3.5 * (height), startY, height);
+	drawA(startX + 4.5 * (height), startY, height);
+	drawR(startX + 5.5 * (height), startY, height);
+	drawT(startX + 6.2 * (height), startY, height);
+	drawH(startX + 7.4* (height), startY+2, height);
+
+	
+}
+void writeAyush(int startX, int startY, int height) {
+	drawA(startX, startY, height);
+	drawY(startX + 0.5 * height, startY, height);
+	drawU(startX + 1.5 * (height), startY, height);
+	drawS(startX + 2.5 * (height), startY, height);
+	drawH(startX + 3.5 * (height), startY, height);
+}
+void writeParam(int startX, int startY, int height) {
+	drawP(startX, startY, height);
+	drawA(startX + 1* height, startY, height);
+	drawR(startX + 2 * (height), startY, height);
+	drawA(startX + 3 * (height), startY, height);
+	drawM(startX + 4 * (height), startY, height);
+}
+void drawBannerWithName(int s) {
+	glBegin(GL_QUADS);
+	glColor3f(1, 1, 1);
+	glVertex2f(25 * s, 15 * s);
+	glVertex2f(25 * s, 36 * s);
+	glVertex2f(230 * s, 40 * s);
+	glVertex2f(230 * s, 20 * s);
+	glEnd();
+	glColor3f(0.8, 0.4, 0.1);
+	writeAyush(65,40 ,20);
+	writeSiddharth(170, 43, 20);
+	writeParam(350, 46, 20);
+}
+void drawPlane(int s) {
+	float sideCol[] = { 0.9,0.4,0 };
+	getSide(3 * s, -33 * s, 17 * s, -40 * s, 32 * s,sideCol);
+	glBegin(GL_QUADS);
+	glColor3f(1, 0, 0);
+	glVertex2f(-33 * s, 20 * s);
+	glVertex2f(-40 * s, 35 * s);
+	glVertex2f(-35 * s, 35 * s);
+	glVertex2f(0 * s, 20 * s);
+	glColor3f(0.3, 0, 0);
+	glVertex2f(-40*s, 12*s);
+	glVertex2f(-40*s, 22*s);
+	glVertex2f(23*s, 27*s);
+	glVertex2f(23*s,23*s );
+	glColor3f(0.6, 0, 0);
+	glVertex2f(-40*s, 22*s);
+	glVertex2f(-45*s, 25*s);
+	glVertex2f(18*s, 28*s);
+	glVertex2f(23*s, 27*s);
+	glColor3f(0.4,0, 0);
+	glVertex2f(-40 * s, 12 * s);
+	glVertex2f(-40 * s, 22 * s);
+	glVertex2f(-49 * s, 17 * s);
+	glVertex2f(-49 * s, 13 * s);
+	glColor3f(0.9, 0, 0);
+	glVertex2f(-49 * s, 13 * s);
+	glVertex2f(-49 * s, 17 * s);
+	glVertex2f(-52 * s, 20 * s);
+	glVertex2f(-52 * s, 16 * s);
+	glColor3f(0.9, 0.9, 0.9);
+	glVertex2f(-49 * s, 17 * s);
+	glVertex2f(-40 * s, 22 * s);
+	glVertex2f(-45 * s, 25 * s);
+	glVertex2f(-52 * s, 20 * s);
+	glColor3f(1, 0, 0);
+	glVertex2f(0*s, 0*s);
+	glVertex2f(-30*s, 20*s);
+	glVertex2f(-10*s, 20 * s);
+	glVertex2f(05*s, 0*s);
+	glEnd();
+	drawBannerWithName(s);
+	glBegin(GL_QUADS);
+	glColor3f(1, 0, 0);
+	glVertex2f(25* s, 15 * s);
+	glVertex2f(10 * s, 25 * s);
+	glVertex2f(20 * s, 25 * s);
+	glVertex2f(28 * s, 15 * s);
+	glColor3f(1, 0, 0);
+	glVertex2f(15 * s, 27 * s);
+	glVertex2f(22 * s,  27* s);
+	glVertex2f(22 * s, 40 * s);
+	glVertex2f(19 * s, 40 * s);
+	glEnd();
+	drawFront(3*s, 5*s, 0*s, -3*s, supportColor);
+	getSide(3*s, 0*s, -3*s,-30*s, 17*s,sideCol);
+	
+
+}
 void myDisplay(void) {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	glColor3f(0.0f, 1, 1);
 	glPushMatrix();
 	//ocean
 	ocean();
+	glPushMatrix();
+	glTranslatef(1700-planeX, 1000-planeY, 0);
+	drawPlane(2);
+	glPopMatrix();
 	///rail
 	railway();
 	railTrack();
@@ -577,7 +786,7 @@ void myDisplay(void) {
 	drawFerrisWheel();
 	lightPole();
 	//train-with-engine
-	drawTrain(trainX);
+	drawTrain(200+trainX);
 	//rail-static-bridge-right-front-portion
 	if (shipX >= 600)
 		drawRightFrontBridge();
@@ -589,6 +798,7 @@ void myDisplay(void) {
 	//rail-static-bridge-left-front-portion
 	drawLeftFrontBridge();
 	glPopMatrix();
+	
 	glFlush();
 }
 void timer(int value) {
@@ -600,6 +810,12 @@ void timer(int value) {
 	pos1++;
 	pos2+=2;
 	pos3+=3;
+	planeX += 3;
+	planeY+= 0.5;
+	if (planeX > 2500) {
+		planeX = 0;
+		planeY = 0;
+	}
 	if (pos1 == sides) {
 		pos1 = 0;
 	}
@@ -610,7 +826,7 @@ void timer(int value) {
 		pos3 = 0;
 	}
 	if (isBridgeUp) {
-		if (shipX <= 1400) {
+		if (shipX <= 1500) {
 			shipX += 4;
 			shipY += 4;
 			if (shipX >= 360) {
@@ -643,7 +859,7 @@ void timer(int value) {
 		}
 	
 	glutPostRedisplay();
-	glutTimerFunc(1, timer, 0);
+	glutTimerFunc(50, timer, 0);
 }
 int main(int argc, char** argv)
 {
@@ -654,7 +870,7 @@ int main(int argc, char** argv)
 	glutCreateWindow("Ship Under Bridge Simulation");
 	myInit();
 	glutDisplayFunc(myDisplay);
-	glutTimerFunc(1, timer, 0);
+	glutTimerFunc(50, timer, 0);
 	glutMainLoop();
 
 }
